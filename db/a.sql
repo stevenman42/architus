9efc7effbba4d57fba40036a786eaf0f7772a00a
stevenman42 <stevenman42@gmail.com>
2019-12-31 20:45:51 -0600

    who cares

--- db/a.sql
+++
@@ -1,8 +0,0 @@
-create table tb_events (
-  guild_id bigint not null,
-  message_id bigint not null,
-  data text not null,
-  expiration_date timestamp without time zone not null,
-  event_type smallint not null,
-  primary key (guild_id, message_id)
-);
